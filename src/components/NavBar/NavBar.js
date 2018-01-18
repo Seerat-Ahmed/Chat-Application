6c4e62a187eccb381bd198c34c629b74fecc555f
Seerat Ahmed Khan <seeratahmed12@gmail.com>
2018-01-18 21:49:13 +0500

    nav bar added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react';
 import './NavBar.css';
 import { connect } from 'react-redux';
-import counteraction from '../../store/actions/counter-action';
-import decrementaction from '../../store/actions/decrement-action';
+import counter from '../../store/actions/counter-action';
+import decrement from '../../store/actions/decrement-action';
 
 class NavBar extends Component {
-    
-    constructor(props) {
-        super(props);
-        console.log('NavBar has been rendered');
-    }
 
-    render(){
-        return( <div>
-                    <button onClick = { () => this.props.counteraction() } className="btn btn-primary">
-                        Increment
-                    </button>
-                    <button onClick = { () => this.props.decrementaction() } className="btn btn-danger">
-                        Decrement
-                    </button>
-                </div>);
-    }
+  constructor(props) {
+    super(props);
+    console.log('NavBar has been rendered');
+  }
 
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="www.google.combs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="www.google.com"><img src={require('../../assets/firebase-logo.png')} className="logo" alt="firebase logo" /></a>
+          </div>
+
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li><a href="www.google.com">Home</a></li>
+              <li><a href="www.google.com">About</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Sign In</a></li>
+              <li><a href="#">Sign Up</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 
 const mapStateToProps = (state) => {
-    return {
-        counter: state.counter,
-    }
+  return {
+    counter: state.counter,
+  }
 }
 
-export default connect(mapStateToProps, { 
-    counteraction: counteraction,
-    decrementaction: decrementaction,
- })(NavBar);
+export default connect(mapStateToProps, {
+  counter: counter,
+  decrement: decrement,
+})(NavBar);
+
+
